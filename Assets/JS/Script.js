eba7647c0482030c4c32b25416ae0b3bae6c844f
M. Hammad Yousuf <74192966+Hammad-Yousuf@users.noreply.github.com>
2021-03-26 10:52:02 +0500

    Add files via upload

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -1,3 +1,22 @@
-function closeHeader () {
-    document.getElementById('header').style.display = 'none'
-}
+$(document).ready(function(){
+    $('#closebtn').click(()=>{
+        $('.header-text').hide()
+    })
+    $('#hidden-products .product-main-container').hide();
+    $('#load-more').click(()=>{
+        $('#hidden-products .product-main-container').fadeIn()
+        $('#load-more').fadeOut()
+    })
+
+})
+
+document.querySelector('.form-button').addEventListener('click', ()=>{
+    function alert(){
+        alert('Thank You our Agent will Contact you Soon')
+    }
+    alert()
+    setTimeout(alert(), 1000)
+})
+
+
+
